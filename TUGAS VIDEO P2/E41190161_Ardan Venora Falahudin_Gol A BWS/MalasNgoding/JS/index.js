26dd6ff910a5f82339273e6340339f64b7814957
ardanvf <71808926+ardanvf@users.noreply.github.com>
2020-10-22 19:26:17 +0700

    Tugas pertemuan ke-5 bootstrap

---
+++ TUGAS VIDEO P2/E41190161_Ardan Venora Falahudin_Gol A BWS/MalasNgoding/JS/index.js
@@ -0,0 +1,13 @@
+// let nama= "Ardan Venora";
+// console.log(nama);
+
+// const age =12;
+// console.log(age);
+
+// const trues = true;
+
+let soal = 1000;
+let jawab = 2000;
+
+soal = soal * jawab;
+console.log(soal);

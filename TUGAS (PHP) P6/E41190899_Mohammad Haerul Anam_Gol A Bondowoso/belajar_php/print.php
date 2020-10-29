92048c83930ff287faa98a577ef1790066994351
mhaerulanam <mhaerulanam603@gmail.com>
2020-10-30 02:22:34 +0700

    Tugas Pertemuan ke-6 yaitu Materi Belajar PHP

---
+++ TUGAS (PHP) P6/E41190899_Mohammad Haerul Anam_Gol A Bondowoso/belajar_php/print.php
@@ -0,0 +1,10 @@
+<?php //kode pembuka php
+
+//Perintah print yang di isi dengan kode HTML yaitu <h2> dan <br>
+print "<h2> Belajar PHP dari Nol! </h2>"; 
+print "Hello World! <br>";
+
+//Perintah print murni syntax PHP dan tidak diselipkan kode HTML
+print "Belajar mencetak teks di PHP!";
+
+?><!-- kode penutup php -->
---
+++ TUGAS (PHP) P6/E41190899_Mohammad Haerul Anam_Gol A Bondowoso/belajar_php/variable.php
@@ -0,0 +1,15 @@
+<?php //kode pembuka php
+
+// membuat variabel baru
+$nama_barang = "Minyak Goreng";
+$harga = 15000;
+
+// Menampilkan isi Variable menggunakan tanda petik dua ("")
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+
+
+// Menampilkan isi Variable menggunakan tanda petik satu ('')
+echo 'Ibu membeli ' .$nama_barang;
+echo ' seharga Rp '  .$harga;
+
+?> <!-- kode penutup php -->

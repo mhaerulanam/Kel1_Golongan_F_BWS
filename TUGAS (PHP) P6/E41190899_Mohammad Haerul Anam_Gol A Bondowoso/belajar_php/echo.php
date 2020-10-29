ed894ee599950c8d4748fec315eacba2d22e4ec1
mhaerulanam <mhaerulanam603@gmail.com>
2020-10-30 02:23:45 +0700

    Ulang

--- TUGAS (PHP) P6/E41190899_Mohammad Haerul Anam_Gol A Bondowoso/belajar_php/echo.php
+++
@@ -1,8 +0,0 @@
-<?php //kode pembuka php
-
-echo "<h2> Belajar PHP itu mudah! </h2>"; // perintah echo dan menyelipkan kode HTMl (<h2>)
-echo ("Hello World! <br>"); // Perintah echo ditambah tanda kurung
-echo "Aku sedang belajar PHP! <br>"; //tanpa tanda kurung dan menyelipkan kode HTML (<br>)
-echo "ini ", "teks ", "yang ", "dibuat ", "terpisah."; //Memasukkan isian lebih dari satu parameter
-
-?> <!-- kode penutup php -->
--- TUGAS (PHP) P6/E41190899_Mohammad Haerul Anam_Gol A Bondowoso/belajar_php/sintax_php.php
+++
@@ -1,6 +0,0 @@
-<?php //kode pembuka php
-
-// syntax untuk menampilkan statement ke layar monitor
-echo "Hello World!"; 
-
-?> <!-- kode penutup php -->

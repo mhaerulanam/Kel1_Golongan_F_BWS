3afc5d782affbcb5e99dd5fb2559f32dbc54c84c
defitamara <71813396+defitamara@users.noreply.github.com>
2020-11-05 13:17:02 +0700

    Update login_action.php

--- TUGAS (Bootstrap Login) P8/E41190942_Defi Tamara_Gol A Bondowoso/login_bootstrap_defi/login_action.php
+++ TUGAS (Bootstrap Login) P8/E41190942_Defi Tamara_Gol A Bondowoso/login_bootstrap_defi/login_action.php
@@ -16,15 +16,39 @@
             'level' => "1"
         ]
     ];
-    foreach($login as $data){
-        if($_POST['user'] == $data['username'] && $_POST['pass'] == $data['password']){
-                session_start();
-                $_SESSION['user'] = $_POST['user'];
-                $_SESSION['level'] = ($data['level']=="1")?'Admin':'User'; //Disini merupakan penentuan. jika 
-                header("Location: dashboard.php");                          //level = 1 maka Admin. Jika bukan maka User
-        }else {
-            echo "Username atau password salah <br>";
-            // die();
-        }    
-    }
+
+    foreach ($login as $data) {
+        if ($_POST['user'] == $data['username']){
+            if ($_POST['pass'] == $data['password']){
+            session_start();
+            $_SESSION['user'] = $_POST['user'];
+            $_SESSION['level'] = ($data['level'] == "1")?'Admin':'Kasir';
+            header("Location: dashboard.php"); //Memindahkan Kehalaman dashboard.php
+            die();
+            }
+            else{
+                echo "<script type='text/javascript'>alert('Password Tidak Benar');</script>";
+                echo "<script type='text/javascript'> window.location.href = 'login.php' </script>";
+            } 
+        } else {
+        // Menampilkan Pesan Error
+        echo "<script type='text/javascript'>alert('Username/Password Tidak Benar');</script>";
+        echo "<script type='text/javascript'> window.location.href = 'login.php' </script>";
+        }
+        // Setelah username dan password melewati 2 perulangan dan tidak ditemukan, maka muncul notifikasi eror 
+}
+
+
+
+    // foreach($login as $data){
+    //     if($_POST['user'] == $data['username'] && $_POST['pass'] == $data['password']){
+    //             session_start();
+    //             $_SESSION['user'] = $_POST['user'];
+    //             $_SESSION['level'] = ($data['level']=="1")?'Admin':'User'; //Disini merupakan penentuan. jika 
+    //             header("Location: dashboard.php");                          //level = 1 maka Admin. Jika bukan maka User
+    //     }else {
+    //         echo "Username atau password salah <br>";
+    //         // die();
+    //     }    
+    // }
 ?>

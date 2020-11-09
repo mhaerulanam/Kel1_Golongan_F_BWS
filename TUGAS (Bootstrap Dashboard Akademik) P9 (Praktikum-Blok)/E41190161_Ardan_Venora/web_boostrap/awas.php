ede28216b91843bf1a95405baddf94c9c1cd00db
ardanvf <71808926+ardanvf@users.noreply.github.com>
2020-11-09 19:05:34 +0700

    Praktek Blok

---
+++ TUGAS (Bootstrap Dashboard Akademik) P9 (Praktikum-Blok)/E41190161_Ardan_Venora/web_boostrap/awas.php
@@ -0,0 +1,19 @@
+<?php
+
+// include "kon.php";
+//                         $name  = $_REQUEST['name'];
+//                         $alm  = $_REQUEST['username'];
+//                         $email  = $_REQUEST['email'];
+//                         $password = $_REQUEST['password'];
+//                         $klamin = @$_REQUEST['klamin'];
+//                         $agama = @$_REQUEST['agama'];
+//                         $bio = @$_REQUEST['bio'];
+//                         $mysqli  = "INSERT INTO mahasiswa (nama, usernam, email, password, gender, agama, biografi) VALUES ('$name', '$alm', '$email', '$password', '$klamin', '$agama', '$bio')";
+//                         $result  = mysqli_query($conn, $mysqli);
+//                         if ($result) {
+//                           echo "Input berhasil";
+//                         } else {
+//                           echo "Input gagal";
+//                         }
+//                         mysqli_close($conn);
+?>

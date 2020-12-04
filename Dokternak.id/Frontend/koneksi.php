ab35558fd67b3b9ed859c4449faa0e107cfd629e
defitamara <71813396+defitamara@users.noreply.github.com>
2020-12-04 13:42:59 +0700

    Update koneksi.php

--- Dokternak.id/Frontend/koneksi.php
+++ Dokternak.id/Frontend/koneksi.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 $koneksi = mysqli_connect("localhost","root","","db_dokternak");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
+
+if(mysqli_connect_error()){
+	echo "Koneksi database gagal : " + mysqli_connect_error();
 }
 ?>

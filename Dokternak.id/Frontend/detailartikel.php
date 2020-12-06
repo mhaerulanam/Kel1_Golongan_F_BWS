a94218398577227c5ffb5ea55d31922a3f43c3a6
defitamara <71813396+defitamara@users.noreply.github.com>
2020-12-06 10:19:56 +0700

    Update detailartikel.php
    Artikel lainnya sudah berfungsi dan ada di side kanan. Jika judul di klik langsung menuju detail artikelnya. Untuk komentar, kategori, pencarian, tags dan lainnya masih belum berfungsi, menunggu code dari member yang lain.

--- Dokternak.id/Frontend/detailartikel.php
+++ Dokternak.id/Frontend/detailartikel.php
@@ -25,7 +25,7 @@
 </head>
 
 <body>
-   <!-- Banner Atas Start-->
+   <!-- Hero Area Start-->
    <div class="slider-area ">
       <div class="single-slider section-overly slider-height2 d-flex align-items-center" data-background="assets/img/gallery/s2.jpg">
           <div class="container">
@@ -39,60 +39,66 @@
           </div>
       </div>
    </div>
-   <!-- Banner End -->
-
-   <!--================Artikel Area =================-->
+   <!-- Hero Area End -->
+   <!--================Blog Area =================-->
    <section class="blog_area single-post-area section-padding">
       <div class="container">
          <div class="row">
-
-
          <?php
-         include "koneksi.php";
+            include "koneksi.php";
 
-         // Disini adalah percabangan if untuk menentukan apakah ada id_artikel yang dibuka
-         // Jika user langsung membuka detailartikel.php tanpa mengklik judul di tipsdantrik.php, maka id_artikel yang ditampilkan otomatis ART1 / Artikel pertama
-         if (isset($_GET["id_artikel"])) {
-            $id_artikel = $_GET["id_artikel"];
-         } else {
-            $id_artikel = $_GET["id_artikel"] = "ART1";
-         }
+            // Konfigurasi menampilkan artikel lainnya
+            $jumlahDataPerHalaman = 3;
+            $result = mysqli_query($koneksi, "SELECT * FROM artikel order by id_artikel desc");
+            $jumlahData = mysqli_num_rows($result);
+            $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+            $halamanAktif = ( isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1;
+
+            // halaman 2, awalDatanya = 2. Dimulai indeks 0,1,2,3, dst
+            $awalData = ( $jumlahDataPerHalaman * $halamanAktif ) - $jumlahDataPerHalaman;
+            // Akhir dari Konfigurasi
+
+            // Disini adalah percabangan if untuk menentukan apakah ada id_artikel yang dibuka
+            // Jika user langsung membuka detailartikel.php tanpa mengklik judul di tipsdantrik.php, maka id_artikel yang ditampilkan otomatis ART1 / Artikel pertama
+            if (isset($_GET["id_artikel"])) {
+               $id_artikel = $_GET["id_artikel"];
+            } else {
+               $id_artikel = $_GET["id_artikel"] = 1;
+            }
          
 
-         // $id_artikel = $_GET['id_artikel'];
-         // $artikelAktif = ( isset($_GET["id_artikel"]) ) ? $_GET["id_artikel"] : ART1;
+            // $id_artikel = $_GET['id_artikel'];
+            // $artikelAktif = ( isset($_GET["id_artikel"]) ) ? $_GET["id_artikel"] : 1;
 
-         $query_mysql = mysqli_query($koneksi,"SELECT * FROM artikel, kategori_artikel WHERE id_artikel = '$id_artikel'
-         AND artikel.id_ktg=kategori_artikel.id_ktg");
-         while ($data = mysqli_fetch_array($query_mysql)) {
-         ?>
-
+            $query_mysql = mysqli_query($koneksi,"SELECT * FROM artikel, kategori_artikel WHERE id_artikel = '$id_artikel'
+            AND artikel.id_ktg=kategori_artikel.id_ktg");
+            while ($data = mysqli_fetch_array($query_mysql)) { ?>
             <div class="col-lg-8 posts-list">
-               <div class="single-post">
-                  <div class="feature-img">
-                     <img class="img-fluid" src="gambar.php?id_artikel=<?php echo $data['id_artikel']; ?>" alt="">
-                  </div>
-                  <div class="blog_details">
-                     <h2><?php echo $data['judul']; ?></h2>
-                     <ul class="blog-info-link mt-3 mb-4">
-                        <li><a href="#"><i class="fa fa-user"></i> <?php echo $data['nama_penulis']; ?></a></li>
-                        <li><a href="#"><i></i>Kategori : <?php echo $data['kategori_artikel']; ?></a></li>
-                        <li><a href="#"><i class="fa fa-comments"></i><?php echo $data['tanggal']; ?></a></li>
-                     </ul>
-                     <p class="excert">
-                        <?php echo nl2br(str_replace(' ', ' ', htmlspecialchars($data['isi']))); ?>
-                     </p>
-               
-                     <div class="quote-wrapper">
-                        <div class="quotes">
-                        <p><?php echo $data['sumber'] ?></p>
-                        </div>
+                  <div class="single-post">
+                     <div class="feature-img">
+                        <img class="img-fluid" src="gambar.php?id_artikel=<?php echo $data['id_artikel']; ?>" alt="">
+                     </div>
+                     <div class="blog_details">
+                           <h2><?php echo $data['judul']; ?></h2>
+                           <ul class="blog-info-link mt-3 mb-4">
+                              <li><a href="#"><i class="fa fa-user"></i> <?php echo $data['nama_penulis']; ?></a></li>
+                              <li><a href="#"><i></i>Kategori : <?php echo $data['kategori_artikel']; ?></a></li>
+                              <li><a href="#"><i class="fa fa-comments"></i><?php echo $data['tanggal']; ?></a></li>
+                           </ul>
+                           <p class="excert">
+                              <?php echo nl2br(str_replace(' ', ' ', htmlspecialchars($data['isi']))); ?>
+                           </p>
+                           <div class="quote-wrapper">
+                              <div class="quotes">
+                              <p><?php echo $data['sumber'] ?></p>
+                           </div>
                      </div>
                   </div>
                </div>
-         <?php } ?>
-
-               <div class="navigation-top">
+                <?php } ?>
+               
+                <!-- Icon Bagikan -->
+                <div class="navigation-top">
                   <div class="d-sm-flex justify-content-between text-center">
                      <div class="col-sm-4 text-center my-2 my-sm-0">
                         <!-- <p class="comment-count"><span class="align-middle"><i class="fa fa-comment"></i></span> 06 Comments</p> -->
@@ -104,9 +110,238 @@
                         <li><a href="#"><i class="fab fa-behance"></i></a></li>
                      </ul>
                   </div>
-  
+               </div>
+
+               <!-- Blog Komentar dan Tulis Komentar -->
+               <div class="comments-area">
+                  <h4>05 Comments</h4>
+                  <div class="comment-list">
+                     <div class="single-comment justify-content-between d-flex">
+                        <div class="user justify-content-between d-flex">
+                           <div class="thumb">
+                              <img src="assets/img/comment/comment_1.png" alt="">
+                           </div>
+                           <div class="desc">
+                              <p class="comment">
+                                 Multiply sea night grass fourth day sea lesser rule open subdue female fill which them
+                                 Blessed, give fill lesser bearing multiply sea night grass fourth day sea lesser
+                              </p>
+                              <div class="d-flex justify-content-between">
+                                 <div class="d-flex align-items-center">
+                                    <h5>
+                                       <a href="#">Emilly Blunt</a>
+                                    </h5>
+                                    <p class="date">December 4, 2017 at 3:12 pm </p>
+                                 </div>
+                                 <div class="reply-btn">
+                                    <a href="#" class="btn-reply text-uppercase">reply</a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                    <div class="comment-form">
+                        <h4>Leave a Reply</h4>
+                        <form class="form-contact comment_form" action="#" id="commentForm">
+                            <div class="row">
+                                <div class="col-12">
+                                <div class="form-group">
+                                    <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9"
+                                        placeholder="Write Comment"></textarea>
+                                </div>
+                                </div>
+                                <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input class="form-control" name="name" id="name" type="text" placeholder="Name">
+                                </div>
+                                </div>
+                                <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input class="form-control" name="email" id="email" type="email" placeholder="Email">
+                                </div>
+                                </div>
+                                <div class="col-12">
+                                <div class="form-group">
+                                    <input class="form-control" name="website" id="website" type="text" placeholder="Website">
+                                </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="button button-contactForm btn_1 boxed-btn">Send Message</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>  
+            </div>
+
+            <!-- Sidebar kanan -->
+            <div class="col-lg-4">
+               <div class="blog_right_sidebar">
+
+               <!-- Pencarian -->
+                  <aside class="single_sidebar_widget search_widget">
+                     <form action="#">
+                        <div class="form-group">
+                           <div class="input-group mb-3">
+                              <input type="text" class="form-control" placeholder='Search Keyword'
+                                 onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Keyword'">
+                              <div class="input-group-append">
+                                 <button class="btns" type="button"><i class="ti-search"></i></button>
+                              </div>
+                           </div>
+                        </div>
+                        <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+                           type="submit">Search</button>
+                     </form>
+                  </aside>
+
+                  <!-- Kategori -->
+                  <aside class="single_sidebar_widget post_category_widget">
+                     <h4 class="widget_title">Category</h4>
+                     <ul class="list cat-list">
+                        <li>
+                           <a href="#" class="d-flex">
+                              <p>Resaurant food</p>
+                              <p>(37)</p>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" class="d-flex">
+                              <p>Travel news</p>
+                              <p>(10)</p>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" class="d-flex">
+                              <p>Modern technology</p>
+                              <p>(03)</p>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" class="d-flex">
+                              <p>Product</p>
+                              <p>(11)</p>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" class="d-flex">
+                              <p>Inspiration</p>
+                              <p>(21)</p>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#" class="d-flex">
+                              <p>Health Care</p>
+                              <p>(21)</p>
+                           </a>
+                        </li>
+                     </ul>
+                  </aside>
+
+                  <!-- Artikel Lainnya -->
+                  <aside class="single_sidebar_widget popular_post_widget">
+                     <h3 class="widget_title">Artikel Lainnya</h3>
+
+                     <?php
+                        $ambilData=mysqli_query($koneksi, "SELECT * FROM artikel, kategori_artikel 
+                            where artikel.id_ktg=kategori_artikel.id_ktg and id_artikel != '$id_artikel' order by id_artikel desc
+                            LIMIT $awalData,$jumlahDataPerHalaman"); 
+                            
+                        while ($data = mysqli_fetch_array($ambilData)) { 
+                    ?>
+                    <div class="media post_item">
+                        <img src="gambar.php?id_artikel=<?php echo $data['id_artikel'];?>" width="120px" />
+                        <div class="media-body">
+                              <a href="detailartikel.php?id_artikel=<?php echo $data['id_artikel']; ?>">
+                                 <h3><?= $data['judul']; ?></h3>
+                              </a>
+                              <p><?= $data['tanggal']; ?></p>
+                        </div>
+                    </div> 
+                    <?php } ?>
+                  </aside>
+                  <aside class="single_sidebar_widget tag_cloud_widget">
+                     <h4 class="widget_title">Tag Clouds</h4>
+                     <ul class="list">
+                        <li>
+                           <a href="#">project</a>
+                        </li>
+                        <li>
+                           <a href="#">love</a>
+                        </li>
+                        <li>
+                           <a href="#">technology</a>
+                        </li>
+                        <li>
+                           <a href="#">travel</a>
+                        </li>
+                        <li>
+                           <a href="#">restaurant</a>
+                        </li>
+                        <li>
+                           <a href="#">life style</a>
+                        </li>
+                        <li>
+                           <a href="#">design</a>
+                        </li>
+                        <li>
+                           <a href="#">illustration</a>
+                        </li>
+                     </ul>
+                  </aside>
+                  <aside class="single_sidebar_widget instagram_feeds">
+                     <h4 class="widget_title">Instagram Feeds</h4>
+                     <ul class="instagram_row flex-wrap">
+                        <li>
+                           <a href="#">
+                              <img class="img-fluid" src="assets/img/post/post_5.png" alt="">
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                              <img class="img-fluid" src="assets/img/post/post_6.png" alt="">
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                              <img class="img-fluid" src="assets/img/post/post_7.png" alt="">
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                              <img class="img-fluid" src="assets/img/post/post_8.png" alt="">
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                              <img class="img-fluid" src="assets/img/post/post_9.png" alt="">
+                           </a>
+                        </li>
+                        <li>
+                           <a href="#">
+                              <img class="img-fluid" src="assets/img/post/post_10.png" alt="">
+                           </a>
+                        </li>
+                     </ul>
+                  </aside>
+                  <aside class="single_sidebar_widget newsletter_widget">
+                     <h4 class="widget_title">Newsletter</h4>
+                     <form action="#">
+                        <div class="form-group">
+                           <input type="email" class="form-control" onfocus="this.placeholder = ''"
+                              onblur="this.placeholder = 'Enter email'" placeholder='Enter email' required>
+                        </div>
+                        <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+                           type="submit">Subscribe</button>
+                     </form>
+                  </aside>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
    <!--================ Blog Area end =================-->
-   
    
 <!-- JS here -->
 	

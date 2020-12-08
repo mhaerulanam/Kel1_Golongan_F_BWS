748d622702bc4a403090cbca9339249e9af51324
Widya Yuristika Oktavia <yuristikawidya@gmail.com>
2020-12-08 15:00:04 +0700

    Daftar Artikel

---
+++ Dokternak.id/Frontend/daftarartikel.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Dokternak.id - Daftar Artikel </title>
+   <meta name="description" content="">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+  
+   <!-- CSS here -->
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+      <link rel="stylesheet" href="assets/css/slicknav.css">
+      <link rel="stylesheet" href="assets/css/price_rangs.css">
+      <link rel="stylesheet" href="assets/css/animate.min.css">
+      <link rel="stylesheet" href="assets/css/magnific-popup.css">
+      <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+      <link rel="stylesheet" href="assets/css/themify-icons.css">
+      <link rel="stylesheet" href="assets/css/slick.css">
+      <link rel="stylesheet" href="assets/css/nice-select.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <section class="blog_area single-post-area section-padding">
+      <div class="container">
+         <div class="row">
+
+                <div class="col-lg-8 mb-5 mb-lg-0">
+                    <div class="blog_left_sidebar">
+                    <?php
+                include "koneksi.php";
+
+                // Belajar paging : https://www.youtube.com/watch?v=Q1xJdoHrTj4
+
+                // Paging - Konfigurasi
+                $jdataperhalaman = 2;
+                $result = mysqli_query($koneksi, "SELECT * FROM artikel");
+                $jumlahData = mysqli_num_rows($result);
+                $jumlahHalaman = ceil($jumlahData / $jdataperhalaman);
+                $halamanAktif = ( isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1 ;
+
+                // halaman 2, awalDatanya = 2. Dimulai indeks 0,1,2,3, dst
+                $awalData = ( $jdataperhalaman * $halamanAktif ) - $jdataperhalaman;
+                // Akhir dari Konfigurasi
+
+                // ambilData adalah variabel untuk menampilkan data dari 2 tabel, yaitu artikel dan kategori_artikel. 
+                // Sehingga kita dapat menampilkan kategorinya, sesuai id_ktg di kedua tabel
+                $ambilData=mysqli_query($koneksi, "SELECT * FROM artikel, kategori_artikel where artikel.id_ktg=kategori_artikel.id_ktg order by id_artikel 
+                LIMIT $awalData,$jdataperhalaman");
+
+
+                while ($data = mysqli_fetch_array($ambilData)) { ?>
+
+                <div class="col-xl-8 col-lg-7 col-md-6">
+                        <div class="home-blog-single mb-30">
+                            <div class="blog-img-cap">
+                                <div class="blog-img">
+                                    <!-- <img src="assets/img/blog/home-blog1.jpg" alt=""> -->
+                                    <!-- Baris img src dibawah ini untuk memanggil gambar sesuai syntax di gambar.php -->
+                                    <img src="gambar.php?id_artikel=<?php echo $data['id_artikel']; ?>"width="700px"/>
+                                    <div class="blog_item_date">
+                                        <span><?php echo $data['tanggal']; ?></span>
+                                        <p>Kategori : <?php echo $data['kategori_artikel']; ?></p>
+                                    </div>
+                                </div>
+                                <div class="blog-cap">
+                                    <p>|   <?php echo $data['nama_penulis']; ?></p>
+                                    <h3><a href="detailartikel.php?id_artikel=<?php echo $data['id_artikel']; ?>"><?php echo $data['judul']; ?></a></h3>
+                                    <a href="detailartikel.php?id_artikel=<?php echo $data['id_artikel']; ?>" class="more-btn">Read more »</a>
+                                </div>
+                            </div>
+                            </div>
+                    <?php } ?>
+                    </div>
+                    </div>
+
+                        <!-- <nav class="blog-pagination justify-content-center d-flex">
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a href="#" class="page-link" aria-label="Previous">
+                                        <i class="ti-angle-left"></i>
+                                    </a>
+                                </li>
+                                <li class="page-item">
+                                    <a href="#" class="page-link">1</a>
+                                </li>
+                                <li class="page-item active">
+                                    <a href="#" class="page-link">2</a>
+                                </li>
+                                <li class="page-item">
+                                    <a href="#" class="page-link" aria-label="Next">
+                                        <i class="ti-angle-right"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav> -->
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="blog_right_sidebar">
+                    <form action="#">
+                    <div class="btn_tulis">
+                            <div class="items-link f-center">
+                                <a href="tulis_artikel.php">++ TULIS ARTIKEL</a>
+                                </div>
+                            </div>
+                            </form>
+                            
+                            <!-- <form action="tulis_artikel.php" method="POST">
+                            <button>Submit</button>
+                            </form>'; -->
+                            
+                        <aside class="single_sidebar_widget search_widget">
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder='Search Keyword'
+                                            onfocus="this.placeholder = ''"
+                                            onblur="this.placeholder = 'Search Keyword'">
+                                        <div class="input-group-append">
+                                            <button class="btns" type="button"><i class="ti-search"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+                                    type="submit">Search</button>
+                            </form>
+                        </aside>
+
+                        <aside class="single_sidebar_widget post_category_widget">
+                            <h4 class="widget_title">KATEGORI</h4>
+                            <ul class="list cat-list">
+                                <li>
+                                    <a href="#" class="d-flex">
+                                        <p>Resaurant food</p>
+                                        <p>(37)</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex">
+                                        <p>Travel news</p>
+                                        <p>(10)</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex">
+                                        <p>Modern technology</p>
+                                        <p>(03)</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex">
+                                        <p>Product</p>
+                                        <p>(11)</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex">
+                                        <p>Inspiration</p>
+                                        <p>21</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex">
+                                        <p>Health Care (21)</p>
+                                        <p>09</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </aside>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--Pagination Start  -->
+<div class="pagination-area pb-115 text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="single-wrap d-flex justify-content-center">
+                            <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-start">
+
+                                    <!-- Memberi tombol prev -->
+                                    <?php if( $halamanAktif > 1) : ?>
+                                        <li class="page-item">
+                                        <a class="page-link" href="?halaman=<?= $halamanAktif - 1; ?>">&lt; Sebelumnya</a></h4>
+                                        </li>
+                                    <?php endif; ?>
+
+                                    <!-- Navigasi Pages -->
+                                    <?php for($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+                                        <?php if ($i == $halamanAktif ) : ?>
+                                            <li class="page-item active">
+                                            <a href="?halaman=<?= $i; ?>" class="page-link"><?= $i; ?></a>
+                                            </li>
+                                        <?php else : ?>
+                                            <li class="page-item">
+                                            <a href="?halaman=<?= $i; ?>" class="page-link"><?= $i; ?></a>
+                                            </li>
+                                        <?php endif; ?>
+                                    <?php endfor; ?>
+
+                                    <!-- Memberi tombol next -->
+                                    <?php if( $halamanAktif < $jumlahHalaman) : ?>
+                                        <li class="page-item">
+                                        <a class="page-link" href="?halaman=<?= $halamanAktif + 1; ?>">Selanjutnya &gt;</span></a>
+                                        </li>
+                                    <?php endif; ?>
+
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- JS here -->
+	
+		<!-- All JS Custom Plugins Link Here here -->
+        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+		<!-- Jquery, Popper, Bootstrap -->
+		<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="./assets/js/popper.min.js"></script>
+        <script src="./assets/js/bootstrap.min.js"></script>
+	    <!-- Jquery Mobile Menu -->
+        <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+		<!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="./assets/js/owl.carousel.min.js"></script>
+        <script src="./assets/js/slick.min.js"></script>
+        <script src="./assets/js/price_rangs.js"></script>
+
+		<!-- One Page, Animated-HeadLin -->
+        <script src="./assets/js/wow.min.js"></script>
+		<script src="./assets/js/animated.headline.js"></script>
+		
+		<!-- Scrollup, nice-select, sticky -->
+        <script src="./assets/js/jquery.scrollUp.min.js"></script>
+        <script src="./assets/js/jquery.nice-select.min.js"></script>
+		<script src="./assets/js/jquery.sticky.js"></script>
+        <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+        <!-- contact js -->
+        <script src="./assets/js/contact.js"></script>
+        <script src="./assets/js/jquery.form.js"></script>
+        <script src="./assets/js/jquery.validate.min.js"></script>
+        <script src="./assets/js/mail-script.js"></script>
+        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+        
+		<!-- Jquery Plugins, main Jquery -->	
+        <script src="./assets/js/plugins.js"></script>
+        <script src="./assets/js/main.js"></script>
+
+</body>
+</html>

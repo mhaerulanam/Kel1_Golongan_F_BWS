30523e3528af316ddb22d77724e34b3d40a248c9
WikeeSriW <71832042+WikeeSriW@users.noreply.github.com>
2020-12-15 22:46:47 +0700

    Update dokter.sql

--- Dokternak.id/Database/dokter/dokter.sql
+++ Dokternak.id/Database/dokter/dokter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2020 at 04:28 PM
+-- Generation Time: Dec 15, 2020 at 04:45 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 

4c4538714098dad6e83f966a325bdaa031da1d2a
ardanvf <71808926+ardanvf@users.noreply.github.com>
2020-12-16 15:31:34 +0700

    Registrasi Peternak

--- Dokternak.id/Frontend/registrasi_peternak.php
+++ Dokternak.id/Frontend/registrasi_peternak.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
+<title>Dokternak.id - Registrasi</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- Font -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+<!-- Script JS -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
 
 /* Modal Content */
 .modal-content {
@@ -112,30 +133,65 @@ body {
   opacity: 1;
   transition: all 0.35s;
 }
-/* The alert message box */
-.alert {
-  padding: 20px;
-  background-color: #f44336; /* Red */
-  color: white;
-  margin-bottom: 15px;
+
+.tabs-2 {
+  position: relative;
+  margin: 1rem 0;
+  background: #ffffff;
+  height: 4rem;
 }
 
-/* The close button */
-.closebtn {
-  margin-left: 15px;
-  color: white;
-  font-weight: bold;
-  float: right;
-  font-size: 22px;
-  line-height: 20px;
-  cursor: pointer;
-  transition: 0.3s;
+.tabs-2::before,
+.tabs-2::after {
+  content: "";
+  display: table;
+}
+.tabs-2::after {
+  clear: both;
 }
 
-/* When moving the mouse over the close button */
-.closebtn:hover {
-  color: black;
+/* FORM */
+form {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+
+.left{
+  position: absolute;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  padding: 40px;
+  width: 300px;
+  height: 400px;
 }
+.right{
+  position: absolute;
+  top: 0;
+  right: 0;
+  box-sizing: border-box;
+  padding: 40px;
+  width: 300px;
+  height: 400px;
+} 
+
+/* Choose File Style */
+
+.input-group {
+  position: relative;
+  margin: 0;
+}
+
+.label--desc {
+  font-size: 20px;
+  color: #999;
+  margin-top: 10px;
+}
+
+.input-file {
+  display: none;
+}  
+
 </style>
 </head>
 <body>
@@ -146,34 +202,39 @@ if(isset($_POST["daftarpeternak"])) {
       $namadepan_peternak = $_POST["namadepan_peternak"];
       $namabelakang_peternak = $_POST["namabelakang_peternak"];
       $email_peternak = $_POST["email_peternak"];
-      $no_hp = $_POST['no_hp'];
-      $no_wa = $_POST['no_wa'];
+      $no_hp = $_POST["no_hp"];
       $jenis_kelamin = $_POST["jenis_kelamin"];
       $alamat = $_POST["alamat"];
-      $fp_name = $_FILES['foto_ternak']['name'];
-      $fp_size = $_FILES['foto_ternak']['size'];
-      $fp_type = $_FILES['foto_ternak']['type'];
+      //$fp_name = $_FILES['foto_ternak']['name'];
+      //$fp_size = $_FILES['foto_ternak']['size'];
+      //$fp_type = $_FILES['foto_ternak']['type'];
       $foto_peternak = $_POST["foto_peternak"];
       $username_peternak = $_POST["username_peternak"];
       $password_peternak = $_POST["password_peternak"];
-      if ($fp_size < 2048000 and ($fp_type =='image/jpeg' or $fp_type == 'image/png'))
+      $id_role = $_POST["id_role"];
+      $angka = 3;
+      $id_role = $angka;
+      echo $id_role;
+      //if ($fp_size < 2048000 and ($fp_type =='image/jpeg' or $fp_type == 'image/png'))
       {
         
-          $fp = addslashes(file_get_contents($_FILES['file_foto']['tmp_name']));
-      $query = "INSERT INTO peternak VALUES('','$namadepan_peternak','$namabelakang_peternak','$email_peternak','$no_hp','$no_wa','$jenis_kelamin','$alamat','$foto_peternak','$username_peternak','$password_peternak')";
+         // $fp = addslashes(file_get_contents($_FILES['file_foto']['tmp_name']));
+      $query = "INSERT INTO peternak VALUES('','$namadepan_peternak','$namabelakang_peternak','$email_peternak','$no_hp','$jenis_kelamin','$alamat','$foto_peternak','$username_peternak','$password_peternak')";
+      $query1 = "INSERT INTO user VALUES('','$namadepan_peternak','$username_peternak','$email_peternak','$alamat','$no_hp','$password_peternak','$foto_peternak','$id_role')";
       mysqli_query($koneksi,$query);
+      mysqli_query($koneksi,$query1);
       //cek keberhasilan
       if(mysqli_affected_rows($koneksi) > 0){
-         echo"Berhasil";
+        header("location:registrasi_dokter.php?pesan=berhasil");
       } else {
-        echo "gagal";
+        header("location:registrasi_dokter.php?pesan=gagal");
       }
 
       }
-      else
-      {
-        header("location:registrasi_dokter.php?pesan=gagal");
-      }
+      //else
+      //{
+      //  header("location:registrasi_dokter.php?pesan=gagal");
+      //}
   }
 
 ?>
@@ -188,8 +249,8 @@ if(isset($_POST["daftarpeternak"])) {
           <label for="tab-1" class="tab-label">Peternak</label>
         <div class="tab-content">
           <div class="modal-body" style="padding:40px 50px;">
-        <form role="form" action="" method="post" action="uploadfotopeternak.php">
-            <center><h2>Registrasi Akun Peternak</h2></center>
+        <form role="form" action="" method="post">
+        <div id="left">
           <div class="form-group">
             <label for="namadepan_peternak"><span class="glyphicon glyphicon-user"></span> Nama Depan</label>
             <input type="text" class="form-control" name="namadepan_peternak" placeholder="Masukkan Nama Depan">
@@ -214,6 +275,7 @@ if(isset($_POST["daftarpeternak"])) {
             <label for="psws"><span class="glyphicon glyphicon-eye-open"></span> Ketik Ulang Password</label>
             <input type="password" class="form-control" name="" placeholder="Ketik ulang password Anda">
           </div>
+        </div>
 
         <!-- Kanan Bang **************************************************************************************-->
         <div id="right">
@@ -221,17 +283,28 @@ if(isset($_POST["daftarpeternak"])) {
             <label for="no_hp"><span class="glyphicon glyphicon-earphone"></span> No Handphone</label>
             <input type="number" class="form-control" name="no_hp" placeholder="Masukkan Nomer Handphone">
           </div>
-          <div class="form-group">
-            <label for="no_wa"><span class="glyphicon glyphicon-earphone"></span> No Whatsapp</label>
-            <input type="number" class="form-control" name="no_wa" placeholder="Masukkan Nomer Whatsapp">
-          </div>
-          <div class="form-group">
+          <!--<div class="form-group">
             <label for="jenis_kelamin"><span class="glyphicon glyphicon-star"></span> Jenis Kelamin</label>
             <br>
             <input type="radio" name="jenis_kelamin" value="Laki-Laki">
             <label for="laki">Laki - Laki</label><br>
             <input type="radio" name="jenis_kelamin" value="Perempuan">
             <label for="laki">Perempuan</label><br>
+          </div> -->
+          <div class="form-group">
+            <label for="jk"><span class="glyphicon glyphicon-star"></span> Jenis Kelamin</label>
+          <div class="wrapper">
+            <div class="tabs-2">
+                <div class="tab">
+                  <input type="radio" name="jenis_kelamin" id="tab-l" class="tab-switch" value="Laki-Laki" selected>
+                  <label for="tab-l" class="tab-label">Laki-Laki</label>
+                </div>
+                <div class="tab">
+                  <input type="radio" name="jenis_kelamin" id="tab-p" class="tab-switch" value="Perempuan" selected>
+                  <label for="tab-p" class="tab-label">Perempuan</label>
+                </div>
+            </div>
+            </div>
           </div>
 
           <div class="form-group">
@@ -239,11 +312,18 @@ if(isset($_POST["daftarpeternak"])) {
             <input type="text" class="form-control" name="alamat" placeholder="Masukkan Alamat Lengkap">
           </div>
 
-          <div class="form-group">
+          <!-- <div class="form-group">
             <label for="foto_peternak"><span class="glyphicon glyphicon-upload"></span> Upload Foto Profil</label>
             <input type="file" class="form-control" name="foto_peternak">
-          </div>
-      </div>
+          </div> -->
+          <label for="sertif"><span class="glyphicon glyphicon-file"></span> Upload Foto Profil</label>
+                            <div class="value">
+                                <div class="input-group js-input-file">
+                                    <input class="input-file" type="file" name="foto_peternak>
+                                </div>
+                                <div class="label--desc">Upload foto profil dengan ukuran maksimal 10 MB</div>
+                            </div>
+        
 
           <div class="form-group">
           
@@ -251,7 +331,8 @@ if(isset($_POST["daftarpeternak"])) {
           <!-- <div class="checkbox">
             <label><input type="checkbox" value="" checked>Menerima Persyaratan Yang Berlaku</label>
           </div> -->
-          <button type="submit" name ="daftarpeternak" class="btn btn-success btn-block"> Daftar</button>
+          <button type="submit" class="btn btn-success btn-block" name="daftarpeternak"><span class="glyphicon glyphicon-check"></span> Daftar</button>
+          </div>
           </form>
           </div>
         </form>
@@ -269,7 +350,7 @@ if(isset($_POST["daftarpeternak"])) {
             <input type="text" class="form-control" id="usrname" placeholder="Enter email">
           </div>
           <div class="checkbox">
-            <label><input type="checkbox" value="" checked>Remember me</label>
+            <label><input type="checkbox" value="" checked>Dengan ini saya menyetujui segala ketentuan dalam website Dokternak.id</label>
           </div>
             <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span>Daftar</button>
         </form>

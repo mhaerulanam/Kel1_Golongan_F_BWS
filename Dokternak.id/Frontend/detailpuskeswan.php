3f54cdf54e5d4d64f09c0680240332650d4814f6
defitamara <71813396+defitamara@users.noreply.github.com>
2020-12-17 16:03:34 +0700

    Update detailpuskeswan.php
    Menambah navbar, footer, dan memperbaiki tampilan pada detail puskeswan.

--- Dokternak.id/Frontend/detailpuskeswan.php
+++ Dokternak.id/Frontend/detailpuskeswan.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
@@ -7,14 +11,29 @@
     <title>Dokternak.id - Detail Puskeswan</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
     <link href="./css_dokter/css/aos.css" rel="stylesheet">
     <link href="./css_dokter/css/bootstrap.min.css" rel="stylesheet">
     <link href="./css_dokter/styles/main.css" rel="stylesheet">
 
+    <style>
+      .capt {
+      margin-top: 0%;
+      margin-bottom: 3%;
+      text-transform: uppercase;
+      font-weight: bold;
+      color: white;
+    }
+    </style>
   </head>
   <body id="top">
-    <div class="page-content">
-      <div>
+
+    <header>
+      <?php include('navbar.php'); ?>
+    </header>
+
+  <div class="page-content">
+      <!-- <div> -->
 
       <!-- Koneksi Database / Pemanggilan Data dari Tabel Dokter -->
       <?php
@@ -41,107 +60,113 @@
 
             while ($d = mysqli_fetch_array($query_pus)) { ?>
 
-<div class="profile-page">
-  <div class="wrapper">
-    <div class="page-header page-header-small" filter-color="green">
-      <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/gallery/s2.jpg');"></div>
+    <div class="profile-page">
+      <div class="wrapper">
+        <div class="page-header page-header-small" filter-color="green">
+          <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/gallery/s2.jpg');"></div>
+          <div class="container">
+            <div class="content-center">
+              <div class="cc-profile-image"><a href="#"><img src="gambar_puskeswan.php?id_puskeswan=<?php echo $d['id_puskeswan']; ?>" alt="Image"/></a></div>
+              <div class="h2 title"><?= $d['nama_puskeswan']; ?></div>
+              <p class="capt"><?= $d['alamat']; ?></p>
+              <a class="genric-btn primary" href="#">Cek Lokasi</a>
+            </div>
+          </div>
+          <div class="section">
+            <div class="button-container">
+              <a class="fab fa-facebook" href="#" rel="tooltip" title="Follow me on Facebook"></a>
+              <a class="fab fa-twitter" href="#" rel="tooltip" title="Follow me on Twitter"></a>
+              <a class="fab fa-google-plus" href="#" rel="tooltip" title="Follow me on Google+"></a>
+              <a class="fab fa-instagram" href="#" rel="tooltip" title="Follow me on Instagram"></a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section" id="about">
       <div class="container">
-        <div class="content-center">
-          <div class="cc-profile-image"><a href="#"><img src="gambar_puskeswan.php?id_puskeswan=<?php echo $d['id_puskeswan']; ?>" alt="Image"/></a></div>
-          <div class="h2 title"><?= $d['nama_puskeswan']; ?></div>
-          <p class="category text-white"><?= $d['alamat']; ?></p>
-          <a class="btn btn-primary smooth-scroll mr-2" href="#" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Cek Lokasi</a>
-        </div>
-      </div>
-      <div class="section">
-        <div class="container">
-          <div class="button-container"><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Facebook"><i class="fa fa-facebook"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Twitter"><i class="fa fa-twitter"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Google+"><i class="fa fa-google-plus"></i></a><a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Instagram"><i class="fa fa-instagram"></i></a></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="section" id="about">
-  <div class="container">
-    <div class="card" data-aos="fade-up" data-aos-offset="10">
-      <div class="row">
-        <div class="col-lg-6 col-md-12">
-          <div class="card-body">
-            <div class="h4 mt-0 title">Jam Kerja</div>
-            <p><?= nl2br(str_replace(' ', ' ', htmlspecialchars($d['jam_kerja']))); ?></p>
-          </div>
-        </div>
-        <div class="col-lg-6 col-md-12">
-          <div class="card-body">
-            <div class="h4 mt-0 title">Lokasi</div>
-            <div class="row">
-              <div class="col-sm-4"><strong class="text-uppercase">Alamat</strong></div>
-              <div class="col-sm-8"><?= $d['alamat']; ?></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<?php } ?>
-
-<div class="section" id="reference">
-  <div class="container cc-reference">
-    <div class="h4 mb-4 text-center title">Tenaga Medis</div>
-    <div class="card" data-aos="zoom-in">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="row">
-
-            <?php while ($d2 = mysqli_fetch_array($query_dp)) { ?>
-              <div class="col-lg-2 col-md-3 cc-reference-header">
-                <img class="rounded-circle z-depth-0" src="gambar_dokter_puskeswan.php?id_dokter=<?php echo $d2['id_dokter']; ?>" alt="Image"/>
-                <div class="h6 pt-2"><?= $d2['nama']; ?></div>
-                <p class="category"><?= $d2['jabatan']; ?></p>
-                <p><?= $d2['tempat']; ?></p>
-                <a class="btn btn-primary" href="detaildokter.php?id_dokter=<?php echo $d2['id_dokter']; ?>" >Detail</a>
-              </div>
-
-              <?php } ?>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="section" id="portfolio">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6 ml-auto mr-auto">
-        <div class="h4 text-center mb-4 title">Dokumentasi</div>
-      </div>
-    </div>
-    <div class="tab-content gallery mt-5">
-      <div class="tab-pane active" id="web-development">
-        <div class="ml-auto mr-auto">
+        <div class="card" data-aos="fade-up" data-aos-offset="10">
           <div class="row">
-
-          <?php while ($d3 = mysqli_fetch_array($query_dokpus)) { ?>
-            <div class="col-md-6">
-              <div class="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#">
-                  <figure class="cc-effect"><img src="gambar_dokumentasi.php?id_dokumentasi=<?php echo $d3['id_dokumentasi']; ?>" alt="Image"/>
-                    <figcaption>
-                      <div class="h4"><?= $d3['judul']; ?></div>
-                      <p><?= $d3['keterangan']; ?></p>
-                    </figcaption>
-                  </figure></a>
+            <div class="col-lg-6 col-md-12">
+              <div class="card-body">
+                <div class="h4 mt-0 title">Jam Kerja</div>
+                <p><?= nl2br(str_replace(' ', ' ', htmlspecialchars($d['jam_kerja']))); ?></p>
               </div>
             </div>
-            <?php } ?>
-
+            <div class="col-lg-6 col-md-12">
+              <div class="card-body">
+                <div class="h4 mt-0 title">Lokasi</div>
+                <div class="row">
+                  <div class="col-sm-4"><strong class="text-uppercase">Alamat</strong></div>
+                  <div class="col-sm-8"><?= $d['alamat']; ?></div>
+                </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+   
+
+    <?php } ?>
+
+    <div class="section" id="reference">
+      <div class="container cc-reference">
+          <div class="h4 mb-4 text-center title">Tenaga Medis</div>
+          <div class="card" data-aos="zoom-in">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="row">
+
+                  <?php while ($d2 = mysqli_fetch_array($query_dp)) { ?>
+                  <div class="col-lg-2 col-md-3 cc-reference-header">
+                    <img class="rounded-circle z-depth-0" src="gambar_dokter_puskeswan.php?id_dokter=<?php echo $d2['id_dokter']; ?>" alt="Image"/>
+                    <div class="h6 pt-2"><b><?= $d2['nama']; ?></b></div>
+                    <p class="category"><?= $d2['jabatan']; ?></p>
+                    <p><?= $d2['tempat']; ?></p>
+                    <a class="genric-btn primary" href="detaildokter.php?id_dokter=<?php echo $d2['id_dokter']; ?>" >Detail</a>
+                  </div>
+
+                  <?php } ?>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    <div class="section" id="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 ml-auto mr-auto">
+            <div class="h4 text-center mb-4 title">Dokumentasi</div>
+          </div>
+        </div>
+        <div class="tab-content gallery mt-5">
+          <div class="tab-pane active" id="web-development">
+            <div class="ml-auto mr-auto">
+              <div class="row">
+
+                <?php while ($d3 = mysqli_fetch_array($query_dokpus)) { ?>
+                  <div class="col-md-6">
+                    <div class="cc-porfolio-image img-raised" data-aos="fade-up" data-aos-anchor-placement="top-bottom"><a href="#">
+                        <figure class="cc-effect"><img src="gambar_dokumentasi.php?id_dokumentasi=<?php echo $d3['id_dokumentasi']; ?>" alt="Image"/>
+                          <figcaption>
+                            <div class="h4"><?= $d3['judul']; ?></div>
+                            <p><?= $d3['keterangan']; ?></p>
+                          </figcaption>
+                        </figure></a>
+                    </div>
+                  </div>
+                  <?php } ?>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><br><br>
 
     <!-- Java Script -->
     <script src="./css_dokter/js/core/jquery.3.2.1.min.js"></script>
@@ -150,5 +175,11 @@
     <script src="./css_dokter/js/now-ui-kit.js?v=1.1.0"></script>
     <script src="./css_dokter/js/aos.js"></script>
     <script src="./css_dokter/scripts/main.js"></script>
+
+    <div class="section">
+        <header>
+          <?php include('footer.php'); ?>
+        </header>
+    </div>
   </body>
 </html>

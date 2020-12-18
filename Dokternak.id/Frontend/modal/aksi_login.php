64cab7611b94819e9a69f913eba4b2f9f7ac6103
mhaerulanam <mhaerulanam603@gmail.com>
2020-12-19 03:39:57 +0700

    Update aksi_login.php
    Menambahkan code link tujuan sesuai dengan user peternak atau dokter ataupun admin

--- Dokternak.id/Frontend/modal/aksi_login.php
+++ Dokternak.id/Frontend/modal/aksi_login.php
@@ -19,29 +19,48 @@
 
 			if ($jumlah>0){
 				$row = mysqli_fetch_assoc($hasil);
-				$nama=$row["nama"];
-				$_SESSION["username"]=$row["nama"];
+				$id = $row["id_user"];
+				$_SESSION["username"]=$row["username"];
 				$_SESSION["id"]=$row["id_user"];
-				$_SESSION["foto"]=$row["foto"];
                 $_SESSION["id_role"]=$row["role"];
 
                 if ($_SESSION["id_role"]=($row["id_role"]==1))
 				{
+					$id = $_SESSION["id"];
+					$sdata1 = "SELECT * FROM admin, user WHERE admin.id_user = '$id' AND admin.id_user=user.id_user";
+					$hasil = mysqli_query($koneksi,$sdata1);
+					$row1 = mysqli_fetch_array($hasil);
+					$nama = $row1["nama"];
                     $level = "Admin";
 					$_SESSION["id_role"] = $level;
-					echo "<script>alert('Selamat $nama, Anda Berhasil Login sebagai $level'); window.location='../../Backend/pages/Dashboard.php'</script>"; 
+					$_SESSION["nama"]=$nama;
+					echo "<script>alert('Selamat $nama, Anda Berhasil Login sebagai $level'); window.location='../LandingPagePeternak.php'</script>"; 
 				} else if ($_SESSION["id_role"]=$row["id_role"]==2)
 				{
+					$id = $_SESSION["id"];
+					$sdata2 = "SELECT * FROM dokter, user WHERE dokter.id_user = '$id' AND dokter.id_user=user.id_user";
+					$hasil = mysqli_query($koneksi,$sdata2);
+					$row2 = mysqli_fetch_array($hasil);
+					$nama = $row2["nama"];
                     $level = "Dokter";
-                    $_SESSION["id_role"] = $level;
-					echo "<script>alert('Selamat $nama, Anda Berhasil Login sebagai $level'); window.location='../dokter/LandingPageDokter.php'</script>"; 
+					$_SESSION["id_role"] = $level;
+					$_SESSION["nama"]=$nama;
+					echo "<script>alert('Selamat $nama, Anda Berhasil Login sebagai $level'); window.location='../LandingPagePeternak.php'</script>"; 
                 }
                 else if ($_SESSION["id_role"]=$row["id_role"]==3)
 				{
+					$id = $_SESSION["id"];
+					$sdata3 = "SELECT * FROM peternak, user WHERE peternak.id_user = '$id' AND peternak.id_user=user.id_user";
+					$hasil = mysqli_query($koneksi,$sdata3);
+					$row3 = mysqli_fetch_array($hasil);
+					$nmd = $row3["namadepan_peternak"];
+					$nmb = $row3["namabelakang_peternak"];
+					$nama = "$nmd $nmb";
                     $level = "Peternak";
-                    $_SESSION["id_role"] = $level;
+					$_SESSION["id_role"] = $level;
+					$_SESSION["nama"]=$nama;
 					echo "<script>alert('Selamat $nama, Anda Berhasil Login sebagai $level'); window.location='../LandingPagePeternak.php'</script>"; 
-				}			
+				}				
 			}else {
                 // alihkan ke halaman login kembali
                 echo "<script>alert('Maaf $nama, Login Anda gagal'); window.location='../LandingPagePeternak.php'</script>"; 

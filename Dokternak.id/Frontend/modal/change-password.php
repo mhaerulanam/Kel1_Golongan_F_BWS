a924eccfe9fb3f6e46a72ff443c4dbb14b266f8b
mhaerulanam <mhaerulanam603@gmail.com>
2020-12-20 03:18:47 +0700

    Update change-password.php
    Mengubah pengaturan penyimpanan untuk perubahan password user peternak

--- Dokternak.id/Frontend/modal/change-password.php
+++ Dokternak.id/Frontend/modal/change-password.php
@@ -2,16 +2,19 @@
 		include '../koneksi.php';
 		session_start();
 		if(isset($_POST['re_password'])){
+		$data="";
 		$id = $_SESSION['id'];	
 		$old_pass=$_POST['old_pass'];
 		$new_pass=$_POST['new_pass'];
 		$re_pass=$_POST['re_pass'];
-		$chg_pwd=mysqli_query($koneksi,"select * from user where id_user='$id'");
-		$chg_pwd1=mysqli_fetch_array($chg_pwd);
-		$data_pwd=$chg_pwd1['password'];
+		$chg_pwd = "SELECT * FROM peternak, user WHERE peternak.id_peternak = '$id' AND peternak.id_user=user.id_user";
+		$hasil = mysqli_query($koneksi,$chg_pwd);
+		$data = mysqli_fetch_array($hasil);
+		$id_user =$data['id_user'];
+		$data_pwd=$data['password'];
 		if($data_pwd==$old_pass){
 			if($new_pass==$re_pass){
-				$update_pwd=mysqli_query($koneksi,"update user set password='$new_pass' where id_user='$id'");
+				$update_pwd=mysqli_query($koneksi,"update user set password='$new_pass' where id_user='$id_user'");
 				echo "<script>alert('Perubahan Password Berhasil'); window.location='../LandingPagePeternak.php'</script>";
 			}
 			else{

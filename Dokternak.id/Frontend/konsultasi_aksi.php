9b36720e1307159d0486fecb7e535e3344014c6e
defitamara <71813396+defitamara@users.noreply.github.com>
2020-12-20 23:39:26 +0700

    Update konsultasi_aksi.php

--- Dokternak.id/Frontend/konsultasi_aksi.php
+++ Dokternak.id/Frontend/konsultasi_aksi.php
@@ -2,20 +2,22 @@
 include "koneksi.php";
 
 
-        $kode = date('His'); //day, month, minutes
+        $kode = date('His'); //Hour,minutes,second
 
         $id_konsultasi = "KONS$kode";
         $id_peternak = $_POST['id_peternak'];
         $id_dokter = $_POST['id_dokter'];
-        $id_kategori = $_POST['id_kategori'];
+        $id_kategori = $_POST['id_kategori']; // ternak atau pets
+        $id_ktg = $_POST['id_ktg']; //Dengan kata lain, ktg disini adalah jenis hewan seperti kucing, kambing, dll
         $nama_hewan = $_POST['nama_hewan'];
         $keluhan = $_POST['keluhan'];
         $tanggal = $_POST['tanggal'];
-        $komentar = $_POST['komentar'];
 
         mysqli_query($koneksi,"insert into konsultasi values 
-        ('$id_konsultasi','$id_peternak','$id_dokter','$id_kategori','$nama_hewan','$keluhan','$tanggal','$komentar')");
+        ('$id_konsultasi','$id_peternak','$id_dokter','$id_kategori','$id_ktg','$nama_hewan','$keluhan','$tanggal')");
         
+        // echo $id_konsultasi, $id_peternak, $id_dokter, $id_kategori, $id_ktg, $nama_hewan, $keluhan, $tanggal;
+
         header("location:form_konsultasi.php?pesan=berhasil");
 
 ?>

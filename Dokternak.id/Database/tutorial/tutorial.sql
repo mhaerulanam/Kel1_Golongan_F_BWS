df756f95f2f7761ceff7f02ed23cf16b1dc827a7
mhaerulanam <mhaerulanam603@gmail.com>
2020-12-22 03:42:16 +0700

    Update tutorial.sql
    Dimohon untuk data icon tutorial di update sesuai dengan data pada tabel ini

--- Dokternak.id/Database/tutorial/tutorial.sql
+++ Dokternak.id/Database/tutorial/tutorial.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Des 2020 pada 21.44
+-- Waktu pembuatan: 21 Des 2020 pada 21.41
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.3
 

5a7e93fc338c83ff5198723348732872795acfba
defitamara <71813396+defitamara@users.noreply.github.com>
2020-12-23 11:10:53 +0700

    Update sertifikasi.php

--- Dokternak.id/Backend/pages/foto/sertifikasi.php
+++ Dokternak.id/Backend/pages/foto/sertifikasi.php
@@ -1,5 +1,5 @@
 <?php
-include('../koneksi.php');
+include('koneksi.php');
 if(isset($_GET['id_dokter'])) 
 {
     $query = mysqli_query($koneksi,"select * from dokter where id_dokter='".$_GET['id_dokter']."'");

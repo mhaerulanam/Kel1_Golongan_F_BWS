ad2a836eca027bf636d89bb88d014cfaaa6851af
WikeeSriW <71832042+WikeeSriW@users.noreply.github.com>
2021-01-04 13:44:02 +0700

    Create gambartutorial.php

---
+++ Dokternak.id/Backend/pages/foto/gambartutorial.php
@@ -0,0 +1,13 @@
+<?php
+include('../koneksi.php');
+if(isset($_GET['id_tutorial'])) 
+{
+    $query = mysqli_query($koneksi,"select * from tutorial where id_tutorial='".$_GET['id_tutorial']."'");
+    $data = mysqli_fetch_array($query);
+    header("Content-type: image");
+    echo $data["icon"];
+}
+else
+{
+    header('location:../tabel_tutorial.php');
+}

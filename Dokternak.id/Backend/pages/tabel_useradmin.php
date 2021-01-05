8c00a7d4c1a1ea207774347e1d4c877984442fc9
mhaerulanam <mhaerulanam603@gmail.com>
2021-01-05 15:42:27 +0700

    Pembuatan fitur cetak laporan user admin

--- Dokternak.id/Backend/pages/tabel_useradmin.php
+++ Dokternak.id/Backend/pages/tabel_useradmin.php
@@ -183,6 +183,7 @@ session_start();
 					</div>
 					<div class="col-sm-6">
 						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">++ Tambah Data	</a>
+						<a href="cetak/cetak_useradmin.php" target="_blank" class="btn btn-info">Cetak</a>
 						<input type="submit" name="deleteall" value="Delete Selected" class="btn btn-danger" onclick="return confirm('Are you sure delete selected records?')">
 					</div>
 				</div>

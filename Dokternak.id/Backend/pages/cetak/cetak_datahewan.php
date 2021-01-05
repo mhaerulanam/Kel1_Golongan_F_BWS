b5a7c6b5b06494ed9accbe99fc2dd8dcc77593b1
Widya Yuristika Oktavia <yuristikawidya@gmail.com>
2021-01-05 14:35:15 +0700

    Create cetak_datahewan.php

---
+++ Dokternak.id/Backend/pages/cetak/cetak_datahewan.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CETAK DATA HEWAN - DOKTERNAK.ID</title>
+</head>
+<body>
+ 
+	<center>
+ 
+		<h2>DATA HEWAN</h2>
+		<h4>Dokternak.id</h4>
+ 
+	</center>
+ 
+	<?php 
+	include 'koneksi.php';
+	?>
+
+	<table border="1">
+                <thead>
+                    <tr>
+						<th>No</th>
+						<th>ID Hewan</th>
+						<th>Nama Hewan</th>
+						<th>Ras Hewan</th>
+						<th>Usia</th>
+                        <th>Keterangan</th>
+                    </tr>
+                </thead>
+                <tbody>
+					<?php
+					$i = 1;
+					$ksql="SELECT * FROM data_hewan";
+					$khasil = mysqli_query($koneksi,$ksql);
+					while($krow = mysqli_fetch_array($khasil))
+					{
+					?>
+
+					<tr>
+						<td>
+						<?= $i ?>
+						</td>
+
+						<!-- Code menampilkan data -->
+                        <td><?php echo $krow['id_hewan']; ?></td>
+						<td><?php echo $krow['nama_hewan']; ?></td>
+						<td><?php echo $krow['ras_hewan']; ?></td>
+						<td><?php echo $krow['usia']; ?></td>
+						<td><?php echo $krow['keterangan']; ?></td>
+                    </tr> 
+					<?php 
+						$i++;
+						}
+						// Close connection
+						mysqli_close($koneksi); 
+					?>
+		</table>
+
+		<script>
+			window.print();
+		</script> 
+	
+ 
+</body>
+</html>

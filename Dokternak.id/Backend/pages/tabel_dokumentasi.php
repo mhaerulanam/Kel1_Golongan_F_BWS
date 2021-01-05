bd33b9bbf1927efe15b9563a370e9aa5b11cf7c2
defitamara <71813396+defitamara@users.noreply.github.com>
2021-01-05 13:57:19 +0700

    Update tabel_dokumentasi.php

--- Dokternak.id/Backend/pages/tabel_dokumentasi.php
+++ Dokternak.id/Backend/pages/tabel_dokumentasi.php
@@ -197,7 +197,7 @@ session_start();
 					</div>
 					<div class="col-sm-6">
 						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">++ Tambah Data	</a>
-						<a href="cetak_dokumentasi.php" target="_blank" class="btn btn-info">Cetak</a>
+						<a href="./cetak/cetak_dokumentasi.php" target="_blank" class="btn btn-info">Cetak</a>
 						<input type="submit" name="deleteall" value="Delete Selected" class="btn btn-danger" onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')">
 					</div>
 				</div>

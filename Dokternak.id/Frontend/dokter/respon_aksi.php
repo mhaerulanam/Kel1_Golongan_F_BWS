7c5b8eb86583c5a3ee49940264f1ddc498237f01
mhaerulanam <mhaerulanam603@gmail.com>
2021-01-10 00:32:36 +0700

    Update respon_aksi.php
    Menambahkan aksi untuk mengubah status_kirim menjadi terespon

--- Dokternak.id/Frontend/dokter/respon_aksi.php
+++ Dokternak.id/Frontend/dokter/respon_aksi.php
@@ -21,9 +21,14 @@ include '../koneksi.php';
         $kirim2 = " insert into riwayat_konsultasi values 
         ('','$idk','$id_respon','$status')";
         mysqli_query($koneksi,$kirim2);
+
+        $ambildata=mysqli_query($koneksi, "SELECT * FROM  riwayat_konsultasi where id_respon = '$id_respon'");
+        $row = mysqli_fetch_array($ambildata);
+        $idss = $row['id_konsultasi'];
+        mysqli_query($koneksi,"update konsultasi set status_kirim='terespon' where id_konsultasi='$idss'");            
+
         
-        echo $id_respon, $id, $respon, $tanggal, $idk, $status;
-            
+        // echo $id_respon, $id, $respon, $tanggal, $idk, $status;
         header("location:respon_konsultasi.php?pesan=berhasil");
     }
 ?>

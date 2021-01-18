df5353850d144284026d371163aaafe961180428
mhaerulanam <mhaerulanam603@gmail.com>
2021-01-19 03:08:10 +0700

    menghapus js

--- Dokternak.id/Frontend/dokter/navbar.php
+++ Dokternak.id/Frontend/dokter/navbar.php
@@ -39,14 +39,17 @@
            <div class="headder-top header-sticky">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-lg-2 col-md-2">
+                        <div class="col-lg-3 col-md-2">
                             <!-- Logo -->
                             <div class="logo">
                                 <a href="LandingPageDokter.php"><img src="../assets/img/logo/logo1.png" alt=""></a>
-                            </div>  
+                            </div> 
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
                         </div>
-                        <div class="col-lg-10 col-md-13">
-                            <div class="menu-wrapper">
+                        <div class="col-lg-9 col-md-9">
+                                <div class="menu-wrapper">
                                 <!-- Main-menu -->
                                 <div class="main-menu">
                                     <nav class="d-none d-lg-block">
@@ -98,49 +101,12 @@
                             </div>
                         </div>
                         <!-- Mobile Menu -->
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
-                        </div>
+                      
                     </div>
                 </div>
            </div>
        </div>
         <!-- Header End -->
     </header>
-    
-		<!-- All JS Custom Plugins Link Here here -->
-        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
-		<!-- Jquery, Popper, Bootstrap -->
-		<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-        <script src="./assets/js/popper.min.js"></script>
-        <script src="./assets/js/bootstrap.min.js"></script>
-	    <!-- Jquery Mobile Menu -->
-        <script src="./assets/js/jquery.slicknav.min.js"></script>
-
-		<!-- Jquery Slick , Owl-Carousel Plugins -->
-        <script src="./assets/js/owl.carousel.min.js"></script>
-        <script src="./assets/js/slick.min.js"></script>
-        <script src="./assets/js/price_rangs.js"></script>
-        
-		<!-- One Page, Animated-HeadLin -->
-        <script src="./assets/js/wow.min.js"></script>
-		<script src="./assets/js/animated.headline.js"></script>
-        <script src="./assets/js/jquery.magnific-popup.js"></script>
-
-		<!-- Scrollup, nice-select, sticky -->
-        <script src="./assets/js/jquery.scrollUp.min.js"></script>
-        <script src="./assets/js/jquery.nice-select.min.js"></script>
-		<script src="./assets/js/jquery.sticky.js"></script>
-        
-        <!-- contact js -->
-        <script src="./assets/js/contact.js"></script>
-        <script src="./assets/js/jquery.form.js"></script>
-        <script src="./assets/js/jquery.validate.min.js"></script>
-        <script src="./assets/js/mail-script.js"></script>
-        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
-        
-		<!-- Jquery Plugins, main Jquery -->	
-        <script src="./assets/js/plugins.js"></script>
-        <script src="./assets/js/main.js"></script>
 </body>
 </html>

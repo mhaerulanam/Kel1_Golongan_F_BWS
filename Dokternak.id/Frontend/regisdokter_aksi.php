0261109670a16235a2709d9945b3b9d1f853eaa6
defitamara <71813396+defitamara@users.noreply.github.com>
2021-01-22 16:59:18 +0700

    Update regisdokter_aksi.php
    Memperbaiki validasi foto profil dan menambah keterangan verifikasi

--- Dokternak.id/Frontend/regisdokter_aksi.php
+++ Dokternak.id/Frontend/regisdokter_aksi.php
@@ -13,6 +13,7 @@ if(isset($_POST['daftar'])) {
 
         $kode = date('His'); //jam,menit,detik
         $id_dokter= "DOC$kode";
+        $verifikasi= "no";
 
         $fp_name = $_FILES['file_foto']['name'];
         $fp_size = $_FILES['file_foto']['size'];
@@ -33,18 +34,23 @@ if(isset($_POST['daftar'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        if ($fp_size < 2048000 and ($fp_type =='image/jpeg' or $fp_type == 'image/png'))
+        if ($fp_size < 2048000 and $fp_type =='image/jpeg' or $fp_type == 'image/png')
         {
-            $fp = addslashes(file_get_contents($_FILES['file_foto']['tmp_name']));
-            $sertif = addslashes(file_get_contents($_FILES['file_sertifikat']['tmp_name']));
-            mysqli_query($koneksi,"insert into dokter values ('$id_dokter','$nama','$email','$jk','$alamat','$tempat','$telpon', '$fp', '$sertif', '$jabatan', '$jadwal_kerja', '$username', '$password')");
+        $fp = addslashes(file_get_contents($_FILES['file_foto']['tmp_name']));
+        $sertif = addslashes(file_get_contents($_FILES['file_sertifikat']['tmp_name']));
+        $sql= "INSERT INTO dokter VALUES ('$id_dokter','$nama','$email','$jk','$alamat','$tempat','$telpon','$fp','$sertif','$jabatan','$jadwal_kerja','$username','$password','$verifikasi')";
+        if(mysqli_query($koneksi,$sql)){
+            session_start();
             // echo $jadwal_kerja, $username, $password,$id_dokter, $nama, $email, $jk, $alamat, $tempat, $telpon, $fp, $sertif, $jabatan ;
             header("location:index.php?pesan=berhasil");
         }else{
             header("location:index.php?pesan=gagal");
         }
+        
+        }else{
+            header("location:index.php?pesan=cek-foto");
     }
-}
+} }
 ?>
 <!-- (id_dokter, nama, email, jenis_kelamin, alamat, tempat, telpon, foto, sertifikasi, id_jabatan, jadwal_kerja, username, password)  -->
 <!-- and $fs_size < 10000000 ($fs_type =='image/jpeg' or $fs_type == 'application/pdf') -->

bc8cfe4b03a13da24b8304fb794577d2f3a4b9e3
WikeeSriW <71832042+WikeeSriW@users.noreply.github.com>
2021-01-23 09:40:05 +0700

    Update konsultasi_aksi.php

--- Dokternak.id/Frontend/konsultasi_aksi.php
+++ Dokternak.id/Frontend/konsultasi_aksi.php
@@ -31,4 +31,23 @@ include "koneksi.php";
 
         header("location:riwayat_konsultasi.php?pesan=berhasil");
 
+        // aksi jenis hewan
+        if (isset($_POST['id_ktg'])){
+                $kategori = $_POST['id_ktg'];
+                $query = mysqli_query($koneksi,"select * from kategori_artikel where kategori_artikel='$kategori'");
+                $data = mysqli_fetch_assoc($query);
+                // menghitung data
+                $jumlah_data1 = mysqli_num_rows($query);
+                    if ( $jumlah_data1 > 0) { 
+                        $id_ktg = $data['id_ktg'];
+                    }else{
+                        $kode = date('His'); //Hour,minutes,second
+                        $id_ktg  = "KAT$kode";
+                        mysqli_query($koneksi,"insert into kategori_artikel  values ('$id_ktg','$kategori')");
+                    }
+                
+            }else{
+                    echo 'no value';
+            }
 ?>
+

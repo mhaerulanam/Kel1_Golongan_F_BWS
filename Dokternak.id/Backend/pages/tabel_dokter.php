e7b280b9b765d0f1f2645ff5f243a38f4bd8253f
defitamara <71813396+defitamara@users.noreply.github.com>
2021-01-23 11:58:56 +0700

    Update tabel_dokter.php
    Menambah tabel dokter yang belum diverifikasi, memfungsikan edit dan hapus pada tabel verifikasi, mengaktifkan verifikasi untuk data dokter, serta menambah tombol batalkan verifikasi pada tabel dokter.

--- Dokternak.id/Backend/pages/tabel_dokter.php
+++ Dokternak.id/Backend/pages/tabel_dokter.php
@@ -266,6 +266,18 @@ session_start();
 
 					}
 
+					// kode aksi batalkan verifikasi
+					if(isset($_POST['batalverifikasi']))
+					{	
+						$sql1 = "UPDATE dokter SET verifikasi = 'no' WHERE id_dokter = '$id_dokter'";			
+						if(mysqli_query($koneksi, $sql1)){
+							echo "<script type='text/javascript'>alert('Pembatalan Verifikasi Berhasil');; window.location='tabel_dokter.php'</script>";
+						} 
+						else{
+							echo "<script type='text/javascript'>alert('Pembatalan Verifikasi gagal'); window.location='tabel_dokter.php'</script>";
+						}
+
+					}
 					?>
 <form method="post" action="">
         <div class="table-wrapper">
@@ -307,7 +319,7 @@ session_start();
 						<th>Jadwal Kerja</th>
 						<th>Username</th>
 						<th>Password</th>
-                        <th>Actions</th>
+                        <th colspan="3">Actions</th>
 
                     </tr>
                 </thead>
@@ -351,10 +363,14 @@ session_start();
 						<td><?php echo $krow['password']; ?></td>
 
 						<!-- Tombol Action -->
-                        <td>
-                            <a href="#editEmployeeModal<?php echo $krow['id_dokter']; ?>" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+						<td>
+							<a href="#editEmployeeModal<?php echo $krow['id_dokter']; ?>" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                             <a href="#deleteEmployeeModal<?php echo $krow['id_dokter']; ?>" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                         </td>
+						<td>
+							<input type="hidden" value="<?php echo $krow['id_dokter']; ?>" name="id_dokter">
+							<input type="submit" class="btn btn-info" value="Batalkan Verifikasi" name="batalverifikasi">
+						</td>
                     </tr>
 					
 
@@ -566,7 +582,112 @@ session_start();
 				</table>
 				</div>
 				<br><br>
-				<h3 class="page-header">Data Dokter (Baru) / Belum Terverifikasi</h3>
+				<?php
+					include 'koneksi.php';
+					// code tombol edit 
+					if(isset($_POST['edit'])){
+						//edit
+						$cek_tabel = mysqli_query($koneksi, "SELECT * FROM dokter WHERE id_dokter='$id_dokter'");
+						while ($cek_gambar = mysqli_fetch_array($cek_tabel)) {
+							$fotoDatabase = $cek_gambar['foto'];
+							$sertifDatabase = $cek_gambar['sertifikasi'];
+						} //end while
+						
+						$foto = $_POST['foto'];
+						$sertifikasi = $_POST['sertifikasi'];
+
+						$lokasiFoto = $_FILES['foto']['tmp_name'];
+						$namaFoto = $_FILES['foto']['name'];
+						$lokasiSertif = $_FILES['sertifikasi']['tmp_name'];
+						$namaSertif = $_FILES['sertifikasi']['name'];
+
+						if (!isset($fotoDatabase) && !isset($sertifDatabase)){
+							if (!$lokasiFoto=="" && !$lokasiSertif==""){
+								$ambilFoto   = addslashes(file_get_contents($_FILES['foto']['tmp_name']));
+								$ambilSertif   = addslashes(file_get_contents($_FILES['sertifikasi']['tmp_name']));
+								$sql = "UPDATE dokter SET nama = '$nama', email = '$email', jenis_kelamin = '$jk', alamat = '$alamat', tempat = '$tempat', telpon = '$telpon', foto = '$ambilFoto', sertifikasi = '$ambilSertif', id_jabatan = '$id_jabatan', jadwal_kerja = '$jadwal_kerja', username = '$username', password = '$password' WHERE id_dokter = '$id_dokter'";
+								if(mysqli_query($koneksi, $sql)){
+									$nilaihasil = "Records updated successfully.";
+								} 
+								else{
+									echo "ERROR: Could not able to execute $sql. " . mysqli_error($koneksi);
+								}
+							}else{
+								?><script> alert ("Masukkan Foto/Sertifikat terlebih dahulu"); </script><?php
+							}
+						}else if(isset($fotoDatabase) && isset($sertifDatabase)){
+							if (!$lokasiFoto=="" && !$lokasiSertif==""){
+								$ambilFoto   = addslashes(file_get_contents($_FILES['foto']['tmp_name']));
+								$ambilSertif   = addslashes(file_get_contents($_FILES['sertifikasi']['tmp_name']));
+								$sql = "UPDATE dokter SET nama = '$nama', email = '$email', jenis_kelamin = '$jk', alamat = '$alamat', tempat = '$tempat', telpon = '$telpon', foto = '$ambilFoto', sertifikasi = '$ambilSertif', id_jabatan = '$id_jabatan', jadwal_kerja = '$jadwal_kerja', username = '$username', password = '$password' WHERE id_dokter = '$id_dokter'";
+								if(mysqli_query($koneksi, $sql)){
+									$nilaihasil = "Records updated successfully.";
+								} 
+								else{
+									echo "ERROR: Could not able to execute $sql. " . mysqli_error($koneksi);
+								}
+							}else if($lokasiFoto=="" && !$lokasiSertif==""){
+								$ambilSertif   = addslashes(file_get_contents($_FILES['sertifikasi']['tmp_name']));
+								$sql = "UPDATE dokter SET nama = '$nama', email = '$email', jenis_kelamin = '$jk', alamat = '$alamat', tempat = '$tempat', telpon = '$telpon', sertifikasi = '$ambilSertif', id_jabatan = '$id_jabatan', jadwal_kerja = '$jadwal_kerja', username = '$username', password = '$password' WHERE id_dokter = '$id_dokter'";
+								if(mysqli_query($koneksi, $sql)){
+									$nilaihasil = "Records updated successfully.";
+								} 
+								else{
+									echo "ERROR: Could not able to execute $sql. " . mysqli_error($koneksi);
+								}
+							}else if (!$lokasiFoto=="" && $lokasiSertif==""){
+								$ambilFoto   = addslashes(file_get_contents($_FILES['foto']['tmp_name']));
+								$sql = "UPDATE dokter SET nama = '$nama', email = '$email', jenis_kelamin = '$jk', alamat = '$alamat', tempat = '$tempat', telpon = '$telpon', foto = '$ambilFoto', id_jabatan = '$id_jabatan', jadwal_kerja = '$jadwal_kerja', username = '$username', password = '$password' WHERE id_dokter = '$id_dokter'";
+								if(mysqli_query($koneksi, $sql)){
+									$nilaihasil = "Records updated successfully.";
+								} 
+								else{
+									echo "ERROR: Could not able to execute $sql. " . mysqli_error($koneksi);
+								}
+							}else{
+								$sql = "UPDATE dokter SET nama = '$nama', email = '$email', jenis_kelamin = '$jk', alamat = '$alamat', tempat = '$tempat', telpon = '$telpon', id_jabatan = '$id_jabatan', jadwal_kerja = '$jadwal_kerja', username = '$username', password = '$password' WHERE id_dokter = '$id_dokter'";
+								if(mysqli_query($koneksi, $sql)){
+									$nilaihasil = "Records updated successfully.";
+								} 
+								else{
+									echo "ERROR: Could not able to execute $sql. " . mysqli_error($koneksi);
+								}
+							}
+						}else{
+							?><script> alert ("Anda harus mengupload foto/sertifikat"); </script><?php
+						}
+						
+					}
+					// ALHAMDULILLAH ***********************************************************************************************************************************************************************************************************************************************************************************************************************************
+
+					//code delete per item
+					if(isset($_POST['delete'])){
+						//delete
+						$sql = "DELETE FROM dokter WHERE id_dokter = '$id_dokter'";
+						if(mysqli_query($koneksi, $sql)){
+							echo "<script type='text/javascript'>alert('Data Dokter Berhasil Dihapus');; window.location='tabel_dokter.php'</script>";
+						} 
+						else{
+							echo "<script type='text/javascript'>alert('Data Dokter Gagal Dihapus');; window.location='tabel_dokter.php'</script>";
+						}
+
+					}
+
+					// kode aksi verifikasi
+					if(isset($_POST['verifikasi']))
+					{	
+						$sql1 = "UPDATE dokter SET verifikasi = 'yes' WHERE id_dokter = '$id_dokter'";			
+						if(mysqli_query($koneksi, $sql1)){
+							echo "<script type='text/javascript'>alert('Verifikasi Berhasil');; window.location='tabel_dokter.php'</script>";
+						} 
+						else{
+							echo "<script type='text/javascript'>alert('Verifikasi gagal'); window.location='tabel_dokter.php'</script>";
+						}
+
+					}
+				?>
+
+				<h3 class="page-header">Data Dokter Baru/<b>Belum Terverifikasi</b></h3>
 
 				<table  class="table table-striped table-hover" >
 					<th>No</th>
@@ -582,7 +703,7 @@ session_start();
 					<th>Jadwal Kerja</th>
 					<th>Username</th>
 					<th>Password</th>
-					<th>Action</th>
+					<th colspan="2">Action</th>
 						<?php
 						$i =1;
 							include "koneksi.php";
@@ -590,6 +711,7 @@ session_start();
 							$jab = mysqli_query($koneksi,$sql);
 							while($data = mysqli_fetch_array($jab))
 							{ ?>
+							<form method="POST" >
 							<tr>
 							<td><?= $i?> </td>
 							<td><?=$data['nama']?> </td>
@@ -607,14 +729,138 @@ session_start();
 							<td><?=$data['username']?> </td>
 							<td><?=$data['password']?> </td>
 							<td>
-                            <a href="#editEmployeeModal<?php echo $krow['id_dokter']; ?>" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                            <a href="#deleteEmployeeModal<?php echo $krow['id_dokter']; ?>" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                        	</td>
+                            	<a href="#editEmployeeModal2<?php echo $data['id_dokter']; ?>" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                            	<a href="#deleteEmployeeModal2<?php echo $data['id_dokter']; ?>" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+							</td>
+							<td>
+								<input type="hidden" value="<?php echo $data['id_dokter']; ?>" name="id_dokter">
+								<input type="submit" class="btn btn-success" value="Verifikasi" name="verifikasi">
+							</td>
 							</tr>
+
+				<!-- MODAL UNTUK DATA BELUM DIVERIFIKASI -->
+				<!-- Edit Modal HTML -->
+				<div id="editEmployeeModal2<?php echo $data['id_dokter']; ?>" class="modal fade">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<form role="form" method="POST" enctype="multipart/form-data">
+									<input type="hidden" class="form-control" value="<?php echo $data['id_dokter']; ?>" name="id_dokter" required>
+									<div class="modal-header">
+										<h4 class="modal-title">Edit</h4>
+										<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									</div>
+									<div class="modal-body">
+										<div class="form-group">
+                                            <label>Nama :</label>
+                                            <input type="text" name="nama" id="nama" class="form-control" value="<?php echo $data['nama']; ?>" >
+                                        </div>
+										<div class="form-group">
+                                            <label>Email :</label>
+                                            <input type="email" name="email" id="email" class="form-control" value="<?php echo $data['email']; ?>" >
+										</div>
+                                        <div class="form-group">
+                                            <label>Jenis Kelamin :</label><br>
+                                            		<div class="radio-inline">
+													<input type="radio" name="jenis_kelamin" id="jk" value="Laki-Laki" <?php if($data['jenis_kelamin']=='Laki-Laki') echo 'checked' ?>> Laki-Laki
+                                                    </div>
+                                                    <div class="radio-inline">
+													<input type="radio" name="jenis_kelamin" id="jk" value="Perempuan" <?php if($data['jenis_kelamin']=='Perempuan') echo 'checked' ?>> Perempuan
+													</div>
+										</div>
+                                        <div class="form-group">
+                                            <label>Alamat :</label>
+                                            <textarea name="alamat" id="alamat" class="form-control" ><?php echo $data['alamat']; ?></textarea>
+										</div>
+										<div class="form-group">
+                                            <label>Tempat :</label>
+                                            <input type="text" name="tempat" id="tempat" class="form-control" value="<?php echo $data['tempat']; ?>" >
+										</div> 
+										<div class="form-group">
+                                            <label>Telpon :</label>
+                                            <input type="text" name="telpon" id="telpon" class="form-control" value="<?php echo $data['telpon']; ?>" >
+										</div>
+                                        <div class="form-group">
+                                            <label>Foto :</label>
+											<img src="foto/foto_dokter.php?id_dokter=<?php echo $data['id_dokter']; ?>"
+											alt="<?php echo "Belum upload foto"; ?>" height="50"></img>
+											<input type="file" name="foto" id="foto" class="form-control"> 
+                                        </div>     
+                                        <div class="form-group">
+                                            <label>Sertifikasi :</label>
+											<img src="foto/sertifikasi.php?id_dokter=<?php echo $data['id_dokter']; ?>"
+											alt="<?php echo "Belum upload sertifikasi"; ?>" height="50"></img>
+                                            <input type="file" name="sertifikasi" id="sertifikasi" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+											<label>Jabatan :</label><br>
+                                            		<div class="radio-inline">
+													<input type="radio" name="id_jabatan" id="id_jabatan" value="J01" <?php if($data['id_jabatan']=='J01') echo 'checked' ?>> Dokter
+                                                    </div>
+                                                    <div class="radio-inline">
+													<input type="radio" name="id_jabatan" id="id_jabatan" value="J02" <?php if($data['id_jabatan']=='J02') echo 'checked' ?>> Paramedis
+													</div>
+													<div class="radio-inline">
+													<input type="radio" name="id_jabatan" id="id_jabatan" value="J03" <?php if($data['id_jabatan']=='J03') echo 'checked' ?>> Petugas IB
+													</div>
+										</div>  
+                                        <div class="form-group">
+                                            <label>Jadwal Kerja :</label>
+                                            <textarea name="jadwal_kerja" id="jadwal_kerja" class="form-control" ><?php echo $data['jadwal_kerja']; ?></textarea>
+										</div>
+                                        <div class="form-group">
+                                            <label>Username :</label>
+                                            <input type="text" name="username" id="username" class="form-control" value="<?php echo $data['username']; ?>"  >
+										</div>    
+										<div class="form-group">
+                                            <label>Password :</label>
+                                            <input type="password" name="password" id="password" class="form-control" value="<?php echo $data['password']; ?>" >
+										</div>
+										<div class="modal-footer">
+											<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+											<input type="submit" class="btn btn-info" value="Save" name="edit">
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+
+
+					<!-- Delete Modal HTML -->
+					<div id="deleteEmployeeModal2<?php echo $data['id_dokter']; ?>" class="modal fade">
+						<div class="modal-dialog">
+							<div class="modal-content">
+							<form method="post" action="">
+								<input type="text" class="form-control" value="<?php echo $data['id_dokter']; ?>" name="id_dokter" required>
+									<div class="modal-header">
+										<h4 class="modal-title">Delete</h4>
+										<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									</div>
+									<div class="modal-body">
+										<p>Apakah anda yakin ingin menghapus data dengan nama <?php echo $data['nama']; ?>?</p>
+										<p class="text-warning"><small>Data yang terhapus tidak dapat dikembalikan.</small></p>
+									</div>
+									<div class="modal-footer">
+										<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+										<input type="submit" class="btn btn-danger" value="Delete" name="delete">
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				<!-- End Modal -->
+
+							</form>
 							<?php
 							$i++;
-						} ?>
+						} 
+							// Close connection
+						mysqli_close($koneksi);
+						?>
+						
+
 				</table>
+
 				</div>
                 </div>
                 <!-- /.container-fluid -->
